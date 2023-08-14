7d60d0444d9dd390c1f79371e45b5bd324ef6c53
pramod <pramodahetti05@gmail.com>
2023-08-14 13:19:39 +0530

    error removed

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,7 +1,6 @@
 const express=require('express')
 const router=express.Router();
 const userauth=require('../../middleware/userauth');
-const posts=require('../../models/Post')
 //get api/users
 //test route
 
